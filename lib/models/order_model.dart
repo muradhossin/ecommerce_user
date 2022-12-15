c5c1b8e3452d386220ce06a77121d954d966a8f6
muradhossin <murad15-2547@diu.edu.bd>
2022-12-15 11:51:49 +0600

    order_model

--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -1,3 +1,57 @@
-class OrderModel{
+import 'package:ecommerce_user/models/address_model.dart';
+import 'package:ecommerce_user/models/cart_model.dart';
+import 'package:ecommerce_user/models/date_model.dart';
 
+const String collectionOrder = 'Order';
+
+const String orderFieldOrderId = 'orderId';
+const String orderFieldUserId = 'userId';
+const String orderFieldGrandTotal = 'grandTotal';
+const String orderFieldDiscount = 'discount';
+const String orderFieldVAT = 'VAT';
+const String orderFieldOrderStatus = 'orderStatus';
+const String orderFieldPaymentMethod = 'paymentMethod';
+const String orderFieldOrderDate = 'orderDate';
+const String orderFieldDeliveryAddress = 'deliveryAddress';
+const String orderFieldProductDetails = 'productDetails';
+
+class OrderModel {
+  String orderId;
+  String userId;
+  String orderStatus;
+  String paymentMethod;
+  num grandTotal;
+  num discount;
+  num VAT;
+  DateModel orderDate;
+  AddressModel deliveryAddress;
+  List<CartModel> productDetails;
+
+  OrderModel(
+      {required this.orderId,
+      required this.userId,
+      required this.orderStatus,
+      required this.paymentMethod,
+      required this.grandTotal,
+      required this.discount,
+      required this.VAT,
+      required this.orderDate,
+      required this.deliveryAddress,
+      required this.productDetails});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      orderFieldOrderId: orderId,
+      orderFieldUserId: userId,
+      orderFieldOrderStatus: orderStatus,
+      orderFieldPaymentMethod: paymentMethod,
+      orderFieldGrandTotal: grandTotal,
+      orderFieldDiscount: discount,
+      orderFieldVAT: VAT,
+      orderFieldOrderDate: orderDate.toMap(),
+      orderFieldDeliveryAddress: deliveryAddress.toMap(),
+      orderFieldProductDetails: List.generate(
+          productDetails.length, (index) => productDetails[index].toMap()),
+    };
+  }
 }
