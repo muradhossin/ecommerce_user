8c5e5273055cb1d2ce389493f7ec4982a7c37502
muradhossin <murad15-2547@diu.edu.bd>
2024-01-03 20:58:16 +0600

    wishlist view added

---
+++ lib/view/wishlist/widgets/wishlist_view.dart
@@ -0,0 +1,82 @@
+import 'package:ecommerce_user/core/constants/constants.dart';
+import 'package:ecommerce_user/core/constants/dimensions.dart';
+import 'package:ecommerce_user/core/extensions/context.dart';
+import 'package:ecommerce_user/core/extensions/style.dart';
+import 'package:ecommerce_user/view/product/models/product_model.dart';
+import 'package:ecommerce_user/view/product/provider/product_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class WishListView extends StatelessWidget {
+  final Function onProductClick;
+  final ProductModel productModel;
+  const WishListView({super.key, required this.onProductClick, required this.productModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onProductClick(),
+      child: Container(
+        padding: const EdgeInsets.all(Dimensions.paddingSmall),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(Dimensions.radiusMedium),
+        ),
+        child: Column(children: [
+
+          Row(
+            children: [
+
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radiusMedium),
+                  image: DecorationImage(
+                    image: NetworkImage(productModel.thumbnailImageModel.imageDownloadUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: Dimensions.paddingLarge),
+
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    Text(
+                      productModel.productName,
+                      style: const TextStyle().semiBold,
+                    ),
+                    const SizedBox(height: Dimensions.paddingSmall),
+
+                    Text(
+                      productModel.shortDescription ?? productModel.longDescription ?? '',
+                      style: context.textTheme.bodyMedium,
+                    ),
+                    const SizedBox(height: Dimensions.paddingSmall),
+
+                    Row(children: [
+                      Text('Price: $currencySymbol${Provider.of<ProductProvider>(context).priceAfterDiscount(productModel.salePrice, productModel.productDiscount)}'),
+                      const SizedBox(width: Dimensions.paddingSmall),
+                      Text('$currencySymbol${productModel.salePrice}', overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.bodyMedium!.copyWith(decoration: TextDecoration.lineThrough)),
+                    ],),
+
+                  ],
+                ),
+              ),
+
+              const SizedBox(width: Dimensions.paddingLarge),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+              ),
+
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/view/wishlist/wishlist_page.dart
+++ lib/view/wishlist/wishlist_page.dart
@@ -1,4 +1,10 @@
+import 'package:ecommerce_user/core/components/no_data_view.dart';
+import 'package:ecommerce_user/core/constants/dimensions.dart';
+import 'package:ecommerce_user/core/routes/app_router.dart';
+import 'package:ecommerce_user/view/wishlist/provider/wishlist_provider.dart';
+import 'package:ecommerce_user/view/wishlist/widgets/wishlist_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WishListPage extends StatelessWidget {
   const WishListPage({super.key});
@@ -7,7 +13,32 @@ class WishListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('My Wishlist'), elevation: 0),
-      body: const Center(child: Text('My Wishlist')),
+      body: Consumer<WishListProvider>(
+        builder: (context, provider, child) {
+          if (provider.wishList.isEmpty) {
+            return const Center(
+              child: NoDataView(),
+            );
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.all(Dimensions.paddingSmall),
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: provider.wishList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: Dimensions.paddingSmall),
+                child: WishListView(
+                  onProductClick: () {
+                    Navigator.pushNamed(context, AppRouter.getProductDetailsRoute(), arguments: provider.wishList[index]);
+                  },
+                  productModel: provider.wishList[index],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
