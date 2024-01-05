4a671f1b3223dc1306b90b603337065019a77933
muradhossin <murad15-2547@diu.edu.bd>
2024-01-05 19:05:10 +0600

    customButton added

--- lib/core/components/custom_button.dart
+++ lib/core/components/custom_button.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  final Color? color;
+  final double? width;
+  final double borderRadius;
+  final double? height;
+  final EdgeInsetsGeometry? padding;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.color,
+    this.width,
+    this.height,
+    this.padding,
+    this.borderRadius = 8,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onPressed(),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color ?? Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
 
